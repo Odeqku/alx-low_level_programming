400ab52a0b65566765eb0a4f43f93335b8894922
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-10-13 17:18:01 -0700

    correction

--- 0x00-hello_world/file.c
+++ 0x00-hello_world/file.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
 
-/*
+
+/**
  *
- *   main - Entry point
+ *  * main - Entry point
  *
+ *   *
  *
+ *    * Return: Always 0 (Success)
  *
- *   Return: Always 0 (Success)
- *
- *
- */
+ *     */
+
 int main(void)
+
 {
-	return (0);
+
+	    return (0);
+
 }
+
+
