0d9aaaf86e682d54deb2134a288ddd7af02b05a2
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-10-15 18:49:12 +0000

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	if (c != 'e' && c != 'q')
+{
+	putchar(c);
+}
+
+{
+	putchar('\n');
+}
+	return (0);
+}
