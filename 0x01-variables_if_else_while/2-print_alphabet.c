bf4f72d8e6b1789c67a9a78f47e8980aad9aab0a
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-10-15 05:09:17 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+	return (0);
+}
