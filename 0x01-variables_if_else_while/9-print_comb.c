d5a0aee8444d163b4988620541a9bdc191bd6f0f
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-10-17 19:33:59 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,16 @@
 
 	for (comb = 0; comb <= 9; comb++)
 {
-	putchar((comb%10) + '0');
+	putchar((comb % 10) + '0');
+
+	if (comb == 9)
+	continue;
+
 
 	putchar(',');
 	putchar(' ');
 }
 	putchar('\n');
+
 	return (0);
 }
