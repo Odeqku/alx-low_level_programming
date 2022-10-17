e129b1eaa12a8ada069e76a0c46c3c490caa34b2
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-10-17 18:39:46 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+
+	int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+{
+	putchar((i%10)+ '0');
+
+
+	putchar(',');
+	putchar(' ');
+}
+	putchar('\n');
+	return (0);
+}
