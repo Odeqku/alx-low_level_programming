4dd5368b80fd4075bb3b836f85bd187b2c9c9f83
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-10-18 21:30:08 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+
+	viod print_alphabet(viod)
+{
+	int a;
+
+	for (m = 'a'; m <= 'z'; m++)
+{
+	putchar (m);
+}
+	putchar('\n');
+}
