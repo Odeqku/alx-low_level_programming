b7aee1f7980736f0a83a07906d01235fae982bbe
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-10-19 00:56:44 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet -
+ *
+ * 
+ * Return: void
+ */
+
+	void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+}
+	_putchar('\n');
+}
