2af8a4573ee5af82e7c2875b8e4441e9881ff1c4
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-10-25 00:00:02 +0000

    task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
-void reset_to_98(int *n);
+void _puts(char *str);
+int _strlen(const char *str);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+void reset_to_98(int *n);
+int _putchar(char c);
