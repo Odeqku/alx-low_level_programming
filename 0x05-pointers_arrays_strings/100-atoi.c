dd350a21b7961d4267e8498ffe8a572e1d3de4f4
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-10-25 00:55:52 +0000

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,18 +10,18 @@ int _atoi(char *s)
 {
 	int sign = 1;
 	unsigned int num = 0;
-	do
-{
-	if (*s == '-')
-	sign *= -1;
 
-	else if (*s >= '0' && *s <= '9')
-	num = (num * 10) + (*s - '0');
+	do {
+		if (*s == '-')
+			sign *= -1;
 
-	else if (num > 0)
-	break;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 
-}	while (*s++);
+		else if (num > 0)
+			break;
+
+	} while (*s++);
 
 	return (num * sign);
 }
