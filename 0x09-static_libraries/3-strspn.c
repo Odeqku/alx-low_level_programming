394dc3d88894b353492b15c01e79bd99b1c16061
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-11-06 02:14:52 +0000

    Task 1

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,29 +1,30 @@
 #include "main.h"
-  2 /**
-  3  * _strspn - gets the length of a prefix substring
-  4  *
-  5  * @s: segment to return bytes from
-  6  * @accept: the bytes to include
-  7  *
-  8  * Return: the number of bytes in the initial segment of @s which consist     only
-  9  * of bytes from @accept
- 10  */
- 11
- 12 unsigned int _strspn(char *s, char *accept)
- 13 {
- 14         int i, j;
- 15         int c = 0;
- 16
- 17         for (i = 0; s[i] != '\0'; i++)
- 18         {
- 19                 if (s[i] != 32)
- 20                 {
- 21                         for (j = 0; accept[j] != '\0'; j++)
- 22                         {
- 23                                 if (s[i] == accept[j])
- 24                                         c++;
- 25                         }
- 26                 }
- 27                 else
- 28                         return (c);
- 29
+  /**
+  * _strspn - gets the length of a prefix substring
+  *
+  * @s: segment to return bytes from
+  * @accept: the bytes to include
+  *
+  * Return: the number of bytes in the initial segment of @s which consist     only
+  * of bytes from @accept
+  */
+
+ unsigned int _strspn(char *s, char *accept)
+ {
+         int i, j;
+         int c = 0;
+
+         for (i = 0; s[i] != '\0'; i++)
+         {
+                 if (s[i] != 32)
+                 {
+                         for (j = 0; accept[j] != '\0'; j++)
+                         {
+                                 if (s[i] == accept[j])
+                                         c++;
+}
+                 }
+                 else
+                         return (c);
+}
+}
