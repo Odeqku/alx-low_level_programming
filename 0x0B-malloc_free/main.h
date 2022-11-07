3b4180005dc29451d72f0cd3f47afd599e29e88b
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-11-07 23:42:08 +0000

    This is main.h

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *argstostr(int ac, char **av);
+char *str_concat(char *s1, char *s2);
+
+#endif
