070f0d9fa05370a65bc56800f36cae2c04c04fa2
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-11-08 00:31:33 +0000

    task 6

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,6 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *argstostr(int ac, char **av);
 char *str_concat(char *s1, char *s2);
+char **strtow(char *str);
 
 #endif
