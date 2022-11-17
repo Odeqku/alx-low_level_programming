f732ebe164f732bf71b7fe0d44f1226d2485bee6
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-11-17 19:46:30 +0000

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,15 +1,35 @@
-    section .data
-msg:	db	"Hello, Holberton",10
-    global main
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_strings - prints string arguments with a separator
+ *
+ * @separator: - thing to print between strings
+ * @n: - number of arguments
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *sep, *ptr;
+	unsigned int i;
+	va_list list;
 
-    section .text
-main:
-    mov	rax, 1
-    mov	rdi, 1
-    mov	rsi, msg
-    mov	rdx, 17
-    syscall
+	if (separator == NULL || *separator == 0)
+		sep = "";
+	else
+		sep = (char *) separator;
+	va_start(list, n);
 
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+	if (n > 0)
+		printf("%s", va_arg(list, char *));
+	for (i = 1; i < n; i++)
+	{
+		ptr = va_arg(list, char*);
+		if (ptr == NULL)
+			ptr = "(nil)";
+		printf("%s%s", sep, ptr);
+	}
+	printf("\n");
+	va_end(list);
+}
