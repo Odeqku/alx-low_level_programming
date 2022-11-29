edc1b10bf7a5d0f527a6f01e00593fa5c9178faf
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-11-29 16:23:24 +0000

    A function that print elements of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+
+/**
+  * print_listint - a function that print elements of listint_t
+  * @h: it takes one argument which is a pointer to listint_t
+  *
+  * Return: an integer in the number of nodes
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t counter = 0;
+	listint_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		counter++;
+	}
+
+	return (counter);
+}
