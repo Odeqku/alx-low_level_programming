68c69137b54aa94caa423410fae497d7d6d1bc32
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-12-11 11:13:30 +0000

    Little adjustment made to code to reflect total compliance with code specifications and quality

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char str[1025];
 
-	int fd = open("Requiescat.txt", O_RDONLY);
+	int fd = open(filename, O_RDONLY| O_CREAT, S_IRUSR | S_IWUSR);
 
 
 	if( fd < 0)
@@ -18,9 +18,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 
-	ssize_t read_num = read(fd, filename,  letters);
+	ssize_t read_num = read(fd, str,  letters);
 
-	ssize_t write_num = write(STDOUT_FILENO,filename, read_num );
+	ssize_t write_num = write(STDOUT_FILENO, str, read_num );
 	
 	if(write_num < read_num)
 	{
