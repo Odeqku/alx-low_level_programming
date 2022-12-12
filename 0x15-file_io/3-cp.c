db153c21d65958beb78af8a16d0b4d1370a08a5d
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-12-12 15:06:40 +0000

    cp (Task 3) and Header FIle

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,77 @@
+/**
+  * main - reads its arguments from the command line
+  * @ac: number of argument read from the cl
+  * @av: the argument(s) read of the cl
+  *
+  * Author: Abdulganiyu Soyoye - Student at Alx-Africa SE
+  * Return: returns 0 always
+  */
+
+
+#include "main.h"
+
+int main(int ac, char **av)
+{
+	ssize_t res;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
+	res = cp_file_from_file_to(av[1], av[2]);
+	printf("-> %li)\n", res);
+
+	return (0);
+}
+
+/**
+  * cp_file_from_file_to - cp the contents from a file to another
+  * @file1: the name of the file to copy from
+  * @file2: the name of the file to copy to
+  *
+  * Return: returns 0 at EOC & 1 upon success of write(),
+  * among other exit() values
+  *
+  * Author: Soyoye Abdulganiyu - Student at Alx-Africa SE
+  */
+
+ssize_t cp_file_from_file_to(const char *file1, const char *file2)
+{
+	ssize_t fp1, fp2, res1, res2, res3;
+	char str[1024];
+
+	fp1 = open(file1, O_RDONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fp2 = open(file2, O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fp1 != 3)
+	{
+		dprintf(2, "Error: Can't read from %s\n", file1);
+
+		exit(98);
+	}
+	if (fp2 < 0)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file2);
+		exit(99);
+	}
+	else
+	{
+		res3 = read(fp1, str, 1024);
+		write(fp2, str, res3);
+		return (1);
+	}
+	res1 = close(fp1);
+	res2 = close(fp2);
+	if (res1 != 0)
+	{
+		dprintf(2, "Error: Can't close fd's %ld\n", fp1);
+		exit(100);
+	}
+	else if (res2 != 0)
+	{
+		dprintf(2, "Error: Can't close fd's %ld\n", fp2);
+		exit(100);
+	}
+	return (0);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef _OKE_
+#define _OKE_
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+ssize_t cp_file_from_file_to(const char *file1, const char *file2);
+#endif
