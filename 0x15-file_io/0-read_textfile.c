86c9f13e9516ead9245760a5d31c34b039f2bdf3
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-12-12 15:37:33 +0000

    Correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,16 +1,23 @@
+/**
+  * ssize_t read_textfile - reads file
+  * @filename: is the file name to be read
+  * @letters: number of characters to be read
+  *
+  * Return: returns number of letters written
+  */
+
+
+
 #include "main.h"
-
-
-
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char str[1025];
 
-	int fd = open(filename, O_RDONLY| O_CREAT, S_IRUSR | S_IWUSR);
+	int fd = open(filename, O_RDONLY | O_CREAT, S_IRUSR | S_IWUSR);
 
 
-	if( fd < 0)
+	if (fd < 0)
 	{
 		perror("Error");
 
@@ -20,14 +27,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	ssize_t read_num = read(fd, str,  letters);
 
-	ssize_t write_num = write(STDOUT_FILENO, str, read_num );
-	
-	if(write_num < read_num)
+	ssize_t write_num = write(STDOUT_FILENO, str, read_num);
+
+	if (write_num < read_num)
 	{
 		return (0);
 	}
 	else if (write_num == read_num)
-		return(read_num);
+		return (read_num);
 
 
 	close(fd);
