ccd8400d26f417a98d1d929f776f107dd79940b2
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-12-12 22:56:55 +0000

    Correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,14 +15,13 @@
 
 int create_file(const char *filename, char *text_content)
 {
-
 	ssize_t len = strlen(text_content);
 
 	int fp;
 
-	if (text_content == NULL)
+	if (strcmp(text_content, "NULL") == 0)
 	{
-		fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		fp = open(filename, O_RDONLY | O_CREAT, S_IRUSR | S_IWUSR);
 		if (fp < 0)
 		{
 			return (-1);
@@ -32,7 +31,6 @@ int create_file(const char *filename, char *text_content)
 			return (1);
 		}
 		close(fp);
-		exit(0);
 	}
 	else
 	{
@@ -54,4 +52,5 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	close(fp);
+	return (0);
 }
