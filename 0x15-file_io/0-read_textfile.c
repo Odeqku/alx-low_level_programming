c1c0e811423247f4f47d036e2366952ecafdd680
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-12-13 11:33:00 +0000

    Correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,13 +12,14 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *str = malloc(sizeof(str) * letters);
+	char *str = malloc(letters * sizeof(char));
+
 	ssize_t read_num;
 	ssize_t write_num;
 
 	int fd = open(filename, O_RDWR | O_CREAT,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-
+	
 
 	if (fd < 0 || (strcmp(filename, "NULL") == 0))
 	{
