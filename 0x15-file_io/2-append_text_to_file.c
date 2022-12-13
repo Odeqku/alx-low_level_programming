7e2f5c981e69e129a686f5e73fc010a1575d4956
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-12-13 19:37:08 +0000

    Correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,8 +1,8 @@
 /**
   * append_text_to_file - adds text to the end of the file
   * @filename: is the variable for the filename
-  * @text_content: is the variable for the text to be appended
-  * 
+  * @text_content: is the variable for the text to be append  * ed
+  *
   * Return: 1 upon success but -1 upon failure
   * Author: Soyoye Abdulganiyu: Alx-Africa SE Student
   */
@@ -11,26 +11,44 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int  fd = open(filename, O_RDWR | O_APPEND);
 
-	int fd = open(filename, O_RDWR | O_APPEND);
-
-/*	printf("%s", text_content);
-*/
-	if (fd < 3)
+/*	printf("%s", text_content);*/
+	if (fd > 0 && strcmp(text_content, "NULL") != 0)
 	{
-		perror("Program");
+		printf("Hi");
+		write(fd, text_content, strlen(text_content));
+		close(fd);
+		return (1);
+	}
+	else if (fd < 0)
+	{
+		printf("%d\n", fd);
+		printf("Hii");
 		return (-1);
-		exit(0);
-	}
-	else
-	{
-		if (text_content != NULL)
-		{
-			write(fd, text_content, strlen(text_content));
-			return (1);
-		}
 	}
 
-	close(fd);
+
+	if (strcmp(filename, "NULL") == 0)
+	{
+		printf("Hiii");
+		return (-1);
+	}
+	else if (fd > 0 && strcmp(text_content, "NULL") == 0)
+	{
+		close(fd);
+		return (1);
+	}
+	else if (strcmp(text_content, "NULL") == 0 && fd < 0)
+	{
+		printf("Hiiii");
+		return (-1);
+	}
+	else if (fd < 0 && strcmp(text_content, "NULL") != 0)
+	{
+		printf("Hiiiii");
+		return (-1);
+	}
+
 	return (0);
 }
