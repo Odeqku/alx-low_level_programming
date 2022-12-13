081b1da5767d7d0ab7d597951d7c3322b1b2c855
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-12-13 16:45:12 +0000

    Correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (strcmp(text_content, "NULL") == 0)
 	{
 		fp = open(filename, O_RDONLY | O_CREAT, S_IRUSR | S_IWUSR);
-		return (0);
+		return (1);
 	}
 
 	if (strcmp(filename, "NULL") == 0)
