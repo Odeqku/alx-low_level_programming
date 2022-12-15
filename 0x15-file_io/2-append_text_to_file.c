d03834c01fae2efd6511c7564e6c96afa2ab531b
Abdulganiyu Soyoye <gsoyoye@gmail.com>
2022-12-15 18:02:55 +0000

    Correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,8 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int  fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd > 0 && strcmp(text_content, "NULL") != 0)
+	if (fd > 0 && (strcmp(text_content, "NULL") != 0 ||
+				strcmp(text_content, "NULL") == 0))
 	{
 		write(fd, text_content, strlen(text_content));
 		close(fd);
